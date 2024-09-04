8cbbd3aa3d5a8b786b5d67d16e7d2c9ff6dac28b
Aoi <a.sakamoto.pr@gmail.com>
2024-09-04 22:18:07 +0900

    Creating data registration function

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkcalendar import DateEntry
+import sqlite3
+
+# Connect SQLite Database(If not exist database file, create database file)
+conn = sqlite3.connect('sapphire.db')
+
+# Create cursor object
+c = conn.cursor()
+
+# Create table book_list
+c.execute('''CREATE TABLE IF NOT EXISTS book_list
+             (no INTEGER PRIMARY KEY AUTOINCREMENT,
+              title TEXT,
+              author TEXT,
+              classification TEXT,
+              read_date TEXT,
+              note TEXT)''')
+
+# 変更を保存
+conn.commit()
 
 class SAPPHIRE:
     def __init__(self, root):
@@ -10,38 +29,54 @@ class SAPPHIRE:
 
         # --------------------First line
         # Register Button
-        self.register_button = tk.Button(root, text="Register", width=15, height=2, command=transition_register_scr)
+        self.register_button = tk.Button(root, text="Register", width=15, height=2, command=lambda: open_register_window(self))
         self.register_button.grid(row=0, column=0, padx=0, pady=20)
 
         # --------------------Second line
-        table_frame = tk.Frame(self.root, bg="whitesmoke", width=1100, height=500)
-        table_frame.grid_propagate(False)
-        table_frame.grid(row=1, column=0, padx=10, pady=10, columnspan=8)
+        self.table_frame = tk.Frame(self.root, bg="whitesmoke", width=1100, height=500)
+        self.table_frame.grid_propagate(False)
+        self.table_frame.grid(row=1, column=0, padx=10, pady=10, columnspan=8)
 
         # Books List
-        books_list = ttk.Treeview(table_frame, columns=("No", "Title", "Author", "Class", "Read Date", "Note"), show="headings")
-
+        self.books_list = ttk.Treeview(self.table_frame, columns=("No", "Title", "Author", "Classification", "Read Date", "Note"), show="headings")
+        
         # Config column header
-        books_list.heading("No", text="No")
-        books_list.heading("Title", text="Title")
-        books_list.heading("Author", text="Author")
-        books_list.heading("Class", text="Class")
-        books_list.heading("Read Date", text="Read Date")
-        books_list.heading("Note", text="Note")
+        self.books_list.heading("No", text="No", anchor="center")
+        self.books_list.heading("Title", text="Title", anchor="center")
+        self.books_list.heading("Author", text="Author", anchor="center")
+        self.books_list.heading("Classification", text="Classification", anchor="center")
+        self.books_list.heading("Read Date", text="Read Date", anchor="center")
+        self.books_list.heading("Note", text="Note", anchor="center")
 
         # Config column width
-        books_list.column("No", width=50)
-        books_list.column("Title", width=300)
-        books_list.column("Author", width=150)
-        books_list.column("Class", width=100)
-        books_list.column("Read Date", width=150)
-        books_list.column("Note", width=350)
+        self.books_list.column("No", width=50, anchor="center")
+        self.books_list.column("Title", width=300, anchor="w")
+        self.books_list.column("Author", width=150, anchor="w")
+        self.books_list.column("Classification", width=100, anchor="center")
+        self.books_list.column("Read Date", width=150, anchor="center")
+        self.books_list.column("Note", width=350, anchor="w")
 
-        books_list.grid(row=0, column=0)
+        self.books_list.grid(row=0, column=0)
+
+        # Display existing books in the database
+        self.load_books()
+
+    def load_books(self):
+        # Clear existing entries in the treeview
+        for row in self.books_list.get_children():
+            self.books_list.delete(row)
+
+        # Fetch data from the database
+        c.execute("SELECT * FROM book_list")
+        books = c.fetchall()
+
+        # Insert data into the treeview
+        for book in books:
+            self.books_list.insert("", "end", values=book)
 
 # Register button
-def transition_register_scr():
-    register_window = tk.Toplevel(root)
+def open_register_window(app):
+    register_window = tk.Toplevel(app.root)
     register_window.title("Register Screen")
     register_window.geometry("1200x650+10+10")
 
@@ -50,27 +85,27 @@ def transition_register_scr():
     label_title.grid(row=0, column=0, padx=10, pady=10)
 
     input_title = tk.Entry(register_window, width=56)
-    input_title.grid(row=0, column=1, padx=10, pady=10)
+    input_title.grid(row=0, column=1, padx=10, pady=10, sticky="w")
 
     # Author
     label_author = tk.Label(register_window, text="Author", width=20, height=2, bg="lightgray")
     label_author.grid(row=1, column=0, padx=10, pady=10)
 
     input_author = tk.Entry(register_window, width=56)
-    input_author.grid(row=1, column=1, padx=10, pady=10)
+    input_author.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
     # Class
-    label_class = tk.Label(register_window, text="Class", width=20, height=2, bg="lightgray")
-    label_class.grid(row=2, column=0, padx=10, pady=10)
+    label_classification = tk.Label(register_window, text="Classification", width=20, height=2, bg="lightgray")
+    label_classification.grid(row=2, column=0, padx=10, pady=10)
 
     # List
     options = ["Scrutiny", "Analysis", "Syntopical"]
 
-    selected_option = tk.StringVar()
-    selected_option.set(options[0])
+    input_classification = tk.StringVar()
+    input_classification.set(options[0])
 
-    option_class = tk.OptionMenu(register_window, selected_option, *options)
-    option_class.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+    option_classification = tk.OptionMenu(register_window, input_classification, *options)
+    option_classification.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
     # Read Date
     label_read_date = tk.Label(register_window, text="Read Date", width=20, height=2, bg="lightgray")
@@ -87,19 +122,36 @@ def transition_register_scr():
     input_note.grid(row=4, column=1, padx=10, pady=10, sticky="w")
 
     # Register Button
-    register_button = tk.Button(register_window, text="Register", width=15, height=2, command=form_register)
+    register_button = tk.Button(register_window, text="Register", width=15, height=2, command=lambda: form_register(input_title, input_author, input_classification, input_read_date, input_note, app))
     register_button.grid(row=5, column=0, padx=10, pady=10)
 
     # Back button
     back_button = tk.Button(register_window, text="Back", width=15, height=2, command=register_window.destroy)
     back_button.grid(row=5, column=1, padx=10, pady=10, sticky="w")
 
-# Form data register function
-def form_register():
-    print("Register!!")
+def form_register(input_title, input_author, input_classification, input_read_date, input_note, app):
+    # Get form data
+    title = input_title.get()
+    author = input_author.get()
+    classification = input_classification.get()
+    read_date = input_read_date.get()
+    note = input_note.get("1.0", "end-1c")
+
+    # Insert form data to database
+    c.execute("INSERT INTO book_list (title, author, classification, read_date, note) VALUES (?, ?, ?, ?, ?)", (title, author, classification, read_date, note))
+
+    # Save changes
+    conn.commit()
+
+    # Notify registration completion
+    tk.messagebox.showinfo("Success", "Data saved to database")
+
+    # Refresh the book list in the main application
+    app.load_books()
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("1200x650+10+10")
     app = SAPPHIRE(root)
     root.mainloop()
+    conn.close()
